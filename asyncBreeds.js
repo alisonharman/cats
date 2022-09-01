1d26a078f741612813bcb76fae618df817ce94a7
Alison Harman <alison_harman@sympatico.ca>
2022-09-01 03:08:21 +0000

    return undefined if does not find matching breed in file

--- asyncBreeds.js
+++ asyncBreeds.js
@@ -5,7 +5,11 @@ const breedDetailsFromFile = function(breed, callback) {
   console.log('breedDetailsFromFile: Calling readFile...');
   fs.readFile(`./data/${breed}.txt`, 'utf8', (error, data) => {
     console.log("In readFile's Callback: it has the data.");
-    if (!error) callback(data);
+    if (!error) 
+      callback(data);
+    else {
+      callback();
+    }
   });
   // ISSUE: Attempting to return data out here will also not work.
   //        Currently not returning anything from here, so breedDetailsFromFile function returns undefined.
@@ -17,3 +21,6 @@ const printOutFileDetails = breed => {
 
 // we try to get the return value
 breedDetailsFromFile('Bombay', printOutFileDetails);
+
+// export the function
+module.exports = breedDetailsFromFile;
